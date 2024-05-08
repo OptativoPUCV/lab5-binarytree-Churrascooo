e987ac97b332509c85620e679a6777d68a7bbe23
Your Name <you@example.com>
2024-05-08 03:28:46 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -107,9 +107,84 @@ TreeNode * minimum(TreeNode * x)
 //------------------------------------------------------------------------
 void removeNode(TreeMap * tree, TreeNode* node)
 {
-    if (tree->root->left != NULL)
+    if (tree == NULL || node == NULL)
     {
-        tree->root->left = NULL;
+        return;
+    }
+    //Esto es cuando no tenga hijos
+    if (node->left == NULL && node->right == NULL)
+    {
+        if (node->parent != NULL)
+        {
+            if (node->parent->left == node)
+            {
+                node->parent->left = NULL;  
+            }
+            else
+            {
+                node->parent->right = NULL;
+            }
+        }
+        else
+        {
+            tree->root = NULL;
+        }
+        free(node);
+        return;
+    }
+    //Este caso es para cuando solo tiene 1 hijo
+    else if (node->left != NULL && node->right == NULL)
+    {
+        if (node->parent != NULL)
+        {
+            if (node->parent->left == node)
+            { 
+                node->parent->left = node->left;
+            }
+
+            else
+            {
+                node->parent->right = node->left;
+            }
+            node->left->parent = node->parent;
+        }
+        else
+        {
+            tree->root = node->left;
+            node->left->parent = NULL;
+        }
+        free(node);
+        return;
+    }
+    else if (node->right != NULL && node->left == NULL)
+    {
+        if (node->parent != NULL)
+        {
+            if (node->parent->left == node)
+            {
+                node->parent->left = node->right;
+            }
+            else
+            {
+                node->parent->right = node->right;
+            }
+            node->right->parent = node->parent;
+        }
+        else
+        {
+            tree->root = node->right;
+            node->right->parent = NULL;
+        }
+        free(node);
+        return;
+    }
+    //Esto es para los casos de cuando tiene 2 hijos
+    else
+    {
+        TreeNode *min = minimum(node->right);
+        node->pair->key = min->pair->key;
+        node->pair->value = min->pair->value;
+        removeNode(tree, min);
     }
 }
 //------------------------------------------------------------------------
