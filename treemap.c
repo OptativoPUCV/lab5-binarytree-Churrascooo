c66797be77419c9484fb2c85286809afb1bbdd14
Your Name <you@example.com>
2024-05-08 20:14:28 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -38,12 +38,61 @@ TreeNode * createTreeNode(void* key, void * value) {
 //------------------------------------------------------------------------
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2))
 {
-    return NULL;
+    TreeMap * new = (TreeMap *)malloc(sizeof(TreeMap));
+    new->root = NULL;
+    new->current = NULL;
+    new->lower_than = lower_than;
+    return new;
 }
 //------------------------------------------------------------------------
 void insertTreeMap(TreeMap * tree, void* key, void * value)
 {
-    
+    if (tree == NULL || key == NULL || value == NULL) return;
+    if (tree->root == NULL)
+    {
+        tree->root = createTreeNode(key, value);
+    }
+    else
+    {
+        TreeNode *aux = tree->root;
+        while (aux != NULL)
+        {
+            if (is_equal(tree, key, aux->pair->key))
+            {
+                return;
+            }
+            if (tree->lower_than(key, aux->pair->key) == 1)
+            {
+                if (aux->left == NULL)
+                {
+                    TreeNode *new = createTreeNode(key, value);
+                    new->parent = aux;
+                    aux->left = new;
+                    tree->current = new;
+                    return;
+                }
+                else
+                {
+                    aux = aux->left;    
+                }
+            }
+            else
+            {
+                if (aux->right == NULL)
+                {
+                    TreeNode *new = createTreeNode(key, value);
+                    new->parent = aux;
+                    aux->right = new;
+                    tree->current = new;
+                    return;
+                }
+                else
+                {
+                    aux = aux->right;
+                }
+            }
+        }
+    }
 }
 //------------------------------------------------------------------------
 TreeNode * minimum(TreeNode * x)
